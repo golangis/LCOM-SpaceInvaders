2f49e334d5a20f87248abeb818a3d9eddfa0e0f4
Pedro Januario <up202108768@fe.up.pt>
2023-02-24 19:44:53 +0000

    timer_test_read_config implemented.

--- lab2/timer.c
+++ lab2/timer.c
@@ -6,10 +6,7 @@
 #include "i8254.h"
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
@@ -40,7 +37,34 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  union timer_status_field_val conf;
-  if (timer_print_config(timer, field, conf) != 0) return 1;
-  return 0;
+  
+  union timer_status_field_val val;
+  
+  bool bcd = !(st % 2);
+  
+  uint8_t programmed_mode = st;
+  uint8_t mask = BIT(3) | BIT(2) | BIT(1);
+  programmed_mode &= mask;
+  programmed_mode >>= 1;
+  
+  uint8_t access = st;
+  mask = BIT(5) | BIT(4);
+  access &= mask;
+  access >>= 4;
+  enum timer_init access_mode;
+  if (access == 1) access_mode = LSB_only;
+  else if (access == 2) access_mode = MSB_only;
+  else if (access == 3) access_mode = MSB_after_LSB;
+  else access_mode = INVAL_val;
+
+  if (field == tsf_all) {
+    val.bcd = bcd;
+    val.count_mode = programmed_mode;
+    val.in_mode = access_mode;
+    val.byte = st;
+  } else if (field == tsf_initial) val.in_mode = access_mode;
+  else if (field == tsf_mode) val.count_mode = programmed_mode;
+  else if (field == tsf_base) val.bcd = bcd;
+
+  return timer_print_config(timer, field, val);
 }
--- lab2/utils.c
+++ lab2/utils.c
@@ -3,17 +3,16 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  val <<= 8;
+  val >>= 8;
+  *lsb = (uint8_t)val;
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  val >>= 8;
+  *msb = (uint8_t)val;
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
