84239b2e18a37f1b98fba8693c68466d48b213ac
Pedro Januario <up202108768@fe.up.pt>
2023-03-23 18:03:42 +0000

    Lab2: ex1 redone.

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -31,45 +31,15 @@ int main(int argc, char *argv[]) {
 }
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-  uint8_t st;
+  uint8_t st = 0;
   if (timer_get_conf(timer, &st) != 0) return 1;
-  if (timer_display_conf(timer, st, field) != 0) return 1;
-  return 0;
+  return timer_display_conf(timer, st, field);
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
-  return timer_set_frequency(timer, freq);
+  return 1;
 }
 
 int(timer_test_int)(uint8_t time) {
-  uint8_t bit_no = 31;
-  int irq_set = BIT(bit_no);
-  int ipc_status;
-  message msg;
-  int r;
-
-  timer_subscribe_int(&bit_no);
-
-  while (time > 0) {
-    if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
-      printf("driver_receive failed with: %d", r);
-      continue;
-    }
-    if (is_ipc_notify(ipc_status)) {
-      switch (_ENDPOINT_P(msg.m_source)) {
-        case HARDWARE:
-          if (msg.m_notify.interrupts & irq_set) {
-            timer_int_handler();
-            if (counter % 60 == 0) {
-              timer_print_elapsed_time();
-              time--;
-            }
-          }
-          break;
-        default: break;
-      }
-    }
-  }
-  timer_unsubscribe_int();
-  return 0;
+  return 1;
 }
--- lab2/utils.c
+++ lab2/utils.c
@@ -3,19 +3,20 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  *lsb = val & 0x00FF;
+  val &= 0x00FF;
+  *lsb = (uint8_t) val;
   return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
   val >>= 8;
-  *msb = val & 0x00FF;
+  *msb = (uint8_t) val;
   return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
   uint32_t v32 = 0;
   if (sys_inb(port, &v32) != 0) return 1;
-  *value = (uint8_t)v32;
+  *value = (uint8_t) v32;
   return 0;
 }
