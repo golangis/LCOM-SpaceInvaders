69f3b708e11368bdfa4ef9668b6c733250e7e110
Pedro Januario <up202108768@fe.up.pt>
2023-04-18 15:30:18 +0100

    Lab5 vg_init() done.

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -39,11 +39,9 @@ int(video_test_init)(uint16_t mode, uint8_t delay) {
 }
 
 int(video_test_rectangle)(uint16_t mode, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color) {
-  /* To be completed */
-  printf("%s(0x%03X, %u, %u, %u, %u, 0x%08x): under construction\n",
-         __func__, mode, x, y, width, height, color);
-
-  return 1;
+  vg_init(mode);
+  //vg_draw_rectangle();
+  return vg_exit();
 }
 
 int(video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first, uint8_t step) {
--- lab5/video.c
+++ lab5/video.c
@@ -1,5 +1,11 @@
 #include <lcom/lcf.h>
 
+static char* video_mem;
+
+static unsigned h_res;
+static unsigned v_res;
+static unsigned bits_per_pixel;
+
 void* (vg_init)(uint16_t mode) {
     vbe_mode_info_t info;
     if (vbe_get_mode_info(mode, &info) != 0) return NULL;
@@ -14,8 +20,6 @@ void* (vg_init)(uint16_t mode) {
         return NULL;
     }
 
-    void *video_mem;
-
     struct minix_mem_range mr;
     unsigned int vram_base = info.PhysBasePtr;
     unsigned int vram_size = info.XResolution * info.YResolution * ((info.BitsPerPixel + 7)/8);
@@ -24,15 +28,25 @@ void* (vg_init)(uint16_t mode) {
     mr.mr_base = (phys_bytes) vram_base;	
     mr.mr_limit = mr.mr_base + vram_size;  
 
-    if( OK != (r = sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr)))
+    if(OK != (r = sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr)))
         panic("sys_privctl (ADD_MEM) failed: %d\n", r);
 
     video_mem = vm_map_phys(SELF, (void *)mr.mr_base, vram_size);
 
-    if(video_mem == MAP_FAILED)
-    panic("couldn't map video memory");
+    if(video_mem == MAP_FAILED) panic("couldn't map video memory");
 
-    //memset(video_mem, 0xff, vram_size);
+    h_res = info.XResolution;
+    v_res = info.YResolution;
+    bits_per_pixel = info.BitsPerPixel;
+
+    memset(video_mem, 0xff, vram_size);
 
     return video_mem;
 }
+
+int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color) {
+    //memset(vg_mem, 0xff);
+    return 1;
+}
+
+int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
