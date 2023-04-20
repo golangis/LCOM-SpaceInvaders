d7508f57574a1b96d2697715dff138f0212df644
Pedro Januario <up202108768@fe.up.pt>
2023-04-20 15:16:38 +0100

    Lab5: progress.

--- lab5/video.c
+++ lab5/video.c
@@ -101,3 +101,7 @@ int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height,
     }
     return 0;
 }
+
+int (vg_draw_matrix)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color) {
+    
+}
