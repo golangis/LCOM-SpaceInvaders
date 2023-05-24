a6be3284110f1353b43f675c9aaaeadfca9ab2ed
Pedro Januario <up202108768@fe.up.pt>
2023-05-24 16:46:18 +0100

    Shield xpm. Adjusted ship position.

--- proj/src/shield.c
+++ proj/src/shield.c
@@ -1,25 +1,38 @@
 #include "shield.h"
 
-Shield* (initShield)(int x_min) {
+Shield* (initShield)(int x_min, int id) {
   Shield* shield = (Shield*) malloc (sizeof(Shield));
   
   shield->x_min = x_min;
   shield->x_max = x_min + 100;
   shield->y_min = 400;
-  shield->y_max = 430;
+  shield->y_max = 432;
   shield->lives = 3;
+  shield->id = id;
 
   return shield;
 }
 
 void (drawShield)(Shield* shield) {
-  if (shield->lives > 0) video_draw_rectangle(shield->x_min, shield->y_min, shield->x_max - shield->x_min, shield->y_max - shield->y_min, 0xffffff);
+  switch (shield->lives) {
+    case 1: video_draw_xpm(shield->x_min, shield->y_min, shield1_xpm); break;
+    case 2: video_draw_xpm(shield->x_min, shield->y_min, shield2_xpm); break;
+    case 3: video_draw_xpm(shield->x_min, shield->y_min, shield3_xpm); break;
+    default: break;
+  }
 }
 
 void (damage)(Shield* shield, enum shot_source src) {
   shield->lives--;
-  switch (src) {
-    case alien: shield->y_min += 10; break;
-    case player: shield->y_max -= 10; break;
+  switch (shield->lives) {
+    case 2: shield->x_min += 7; shield->x_max -= 7; shield->y_min += 5; shield->y_max -= 5; break;
+    case 1: shield->x_min += 7; shield->x_max -= 7; shield->y_min += 6; shield->y_max -= 6; break;
+    default: break;
   }
 }
+
+/*
+3 vidas: 100x32
+2 vidas: 86x22
+1 vida : 71x10
+*/
--- proj/src/shield.h
+++ proj/src/shield.h
@@ -5,15 +5,18 @@
 
 #include "shot.h"
 
+#include "xpm/shield.xpm"
+
 typedef struct {
     int x_min;
     int x_max;
     int y_min;
     int y_max;
     int lives;
+    int id;
 } Shield;
 
-Shield* (initShield)(int x_min);
+Shield* (initShield)(int x_min, int id);
 void (drawShield)(Shield* shield);
 void (damage)(Shield* shield, enum shot_source src);
 
